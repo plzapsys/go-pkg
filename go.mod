5eeca28436b2bd249599efc9a55411daa20a7e52
plzaps <plzaps@gmail.com>
2023-04-26 12:44:19 +0300

    add

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/ahmetb/go-linq/v3 v3.2.0
+	github.com/gin-gonic/gin v1.9.0
 	github.com/go-oauth2/oauth2/v4 v4.5.2
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-resty/resty/v2 v2.7.0
@@ -31,16 +32,25 @@ require (
 )
 
 require (
+	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-test/deep v1.1.0 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -51,13 +61,18 @@ require (
 	github.com/nats-io/nats.go v1.25.0 // indirect
 	github.com/nats-io/nkeys v0.4.4 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opentelemetry.io/otel/metric v0.37.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ problem-details/problem_details.go
@@ -0,0 +1,253 @@
+package problem
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+)
+
+type ProblemDetail struct {
+	Status     int    `json:"status,omitempty"`
+	Title      string `json:"title,omitempty"`
+	Detail     string `json:"detail,omitempty"`
+	Type       string `json:"type,omitempty"`
+	Instance   string `json:"instance,omitempty"`
+	StackTrace string `json:"stackTrace,omitempty"`
+}
+
+var mappers = map[reflect.Type]func() ProblemDetailErr{}
+var mapperStatus = map[int]func() ProblemDetailErr{}
+
+// ProblemDetailErr ProblemDetail error interface
+type ProblemDetailErr interface {
+	SetStatus(status int) ProblemDetailErr
+	GetStatus() int
+	SetTitle(title string) ProblemDetailErr
+	GetTitle() string
+	SetDetail(detail string) ProblemDetailErr
+	GetDetails() string
+	SetType(typ string) ProblemDetailErr
+	GetType() string
+	SetInstance(instance string) ProblemDetailErr
+	GetInstance() string
+	SetStackTrace(stackTrace string) ProblemDetailErr
+	GetStackTrace() string
+}
+
+func (p *ProblemDetail) SetDetail(detail string) ProblemDetailErr {
+	p.Detail = detail
+
+	return p
+}
+
+func (p *ProblemDetail) GetDetails() string {
+	return p.Detail
+}
+
+func (p *ProblemDetail) SetStatus(status int) ProblemDetailErr {
+	p.Status = status
+
+	return p
+}
+
+func (p *ProblemDetail) GetStatus() int {
+	return p.Status
+}
+
+func (p *ProblemDetail) SetTitle(title string) ProblemDetailErr {
+	p.Title = title
+
+	return p
+}
+
+func (p *ProblemDetail) GetTitle() string {
+	return p.Title
+}
+
+func (p *ProblemDetail) SetType(typ string) ProblemDetailErr {
+	p.Type = typ
+
+	return p
+}
+
+func (p *ProblemDetail) GetType() string {
+	return p.Type
+}
+
+func (p *ProblemDetail) SetInstance(instance string) ProblemDetailErr {
+	p.Instance = instance
+
+	return p
+}
+
+func (p *ProblemDetail) GetInstance() string {
+	return p.Instance
+}
+
+func (p *ProblemDetail) SetStackTrace(stackTrace string) ProblemDetailErr {
+	p.StackTrace = stackTrace
+
+	return p
+}
+
+func (p *ProblemDetail) GetStackTrace() string {
+	return p.StackTrace
+}
+
+func writeTo(w http.ResponseWriter, p ProblemDetailErr) (int, error) {
+
+	w.Header().Set("Content-Type", "application/problem+json")
+	w.WriteHeader(p.GetStatus())
+
+	val, err := json.Marshal(p)
+	if err != nil {
+		return 0, err
+	}
+	return w.Write(val)
+}
+
+// MapStatus map status code to problem details error
+func MapStatus(statusCode int, funcProblem func() ProblemDetailErr) {
+	mapperStatus[statusCode] = funcProblem
+}
+
+// Map map custom type error to problem details error
+func Map[T error](funcProblem func() ProblemDetailErr) {
+	mappers[reflect.TypeOf(*new(T))] = funcProblem
+}
+
+// ResolveProblemDetails retrieve and resolve error with format problem details error
+func ResolveProblemDetails(w http.ResponseWriter, r *http.Request, err error) (ProblemDetailErr, error) {
+
+	var statusCode int = http.StatusInternalServerError
+	var echoError *echo.HTTPError
+	var ginError *gin.Error
+
+	if errors.As(err, &echoError) {
+		statusCode = err.(*echo.HTTPError).Code
+		err = err.(*echo.HTTPError).Message.(error)
+	} else if errors.As(err, &ginError) {
+		var rw, ok = w.(gin.ResponseWriter)
+		if ok && rw.Written() {
+			statusCode = rw.Status()
+		}
+		if gin.Mode() == gin.TestMode {
+			var rw = w.(*httptest.ResponseRecorder)
+			if rw.Code != http.StatusOK {
+				statusCode = rw.Code
+			}
+		}
+		err = err.(*gin.Error).Err.(error)
+	}
+
+	var mapCustomType, mapCustomTypeErr = setMapCustomType(w, r, err)
+	if mapCustomType != nil {
+		return mapCustomType, mapCustomTypeErr
+	}
+
+	var mapStatus, mapStatusErr = setMapStatusCode(w, r, err, statusCode)
+	if mapStatus != nil {
+		return mapStatus, mapStatusErr
+	}
+
+	var p, errr = setDefaultProblemDetails(w, r, err, statusCode)
+	if errr != nil {
+		return nil, err
+	}
+	return p, errr
+}
+
+func setMapCustomType(w http.ResponseWriter, r *http.Request, err error) (ProblemDetailErr, error) {
+
+	problemCustomType := mappers[reflect.TypeOf(err)]
+	if problemCustomType != nil {
+		prob := problemCustomType()
+
+		validationProblems(prob, err, r)
+
+		for k, v := range mapperStatus {
+			if k == prob.GetStatus() {
+				_, err = writeTo(w, v())
+				if err != nil {
+					return nil, err
+				}
+				return prob, err
+			}
+		}
+
+		_, err = writeTo(w, prob)
+		if err != nil {
+			return nil, err
+		}
+		return prob, err
+	}
+	return nil, err
+}
+
+func setMapStatusCode(w http.ResponseWriter, r *http.Request, err error, statusCode int) (ProblemDetailErr, error) {
+	problemStatus := mapperStatus[statusCode]
+	if problemStatus != nil {
+		prob := problemStatus()
+		validationProblems(prob, err, r)
+		_, err = writeTo(w, prob)
+		if err != nil {
+			return nil, err
+		}
+		return prob, err
+	}
+	return nil, err
+}
+
+func setDefaultProblemDetails(w http.ResponseWriter, r *http.Request, err error, statusCode int) (ProblemDetailErr, error) {
+	defaultProblem := func() ProblemDetailErr {
+		return &ProblemDetail{
+			Type:       getDefaultType(statusCode),
+			Status:     statusCode,
+			Detail:     err.Error(),
+			Title:      http.StatusText(statusCode),
+			Instance:   r.URL.RequestURI(),
+			StackTrace: errorsWithStack(err),
+		}
+	}
+	prob := defaultProblem()
+	_, err = writeTo(w, prob)
+	if err != nil {
+		return nil, err
+	}
+	return prob, err
+}
+
+func validationProblems(problem ProblemDetailErr, err error, r *http.Request) {
+	problem.SetDetail(err.Error())
+
+	if problem.GetStatus() == 0 {
+		problem.SetStatus(http.StatusInternalServerError)
+	}
+	if problem.GetInstance() == "" {
+		problem.SetInstance(r.URL.RequestURI())
+	}
+	if problem.GetType() == "" {
+		problem.SetType(getDefaultType(problem.GetStatus()))
+	}
+	if problem.GetTitle() == "" {
+		problem.SetTitle(http.StatusText(problem.GetStatus()))
+	}
+	if problem.GetStackTrace() == "" {
+		problem.SetStackTrace(errorsWithStack(err))
+	}
+}
+
+func getDefaultType(statusCode int) string {
+	return fmt.Sprintf("https://httpstatuses.io/%d", statusCode)
+}
+
+func errorsWithStack(err error) string {
+	res := fmt.Sprintf("%+v", err)
+	return res
+}
